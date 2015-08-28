941e166d83c19a25c99f7c69b6a67fc875012a70
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2015-08-28 14:40:45 +0800

    New changes in add form

--- resources/views/dashboard/add.blade.php
+++ resources/views/dashboard/add.blade.php
@@ -161,10 +161,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
 <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-<<<<<<< HEAD
-<script type="text/javascript" src="{{ asset('js/daTables.responsive.min.js') }}"></script>
-=======
->>>>>>> origin/master
 <script type="text/javascript">
 $(document).ready(function(){
     $('#datetimepicker3').datetimepicker({
