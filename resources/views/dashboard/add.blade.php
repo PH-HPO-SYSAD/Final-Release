babd5d00a96c4c6b51a424e4db255d591f5ed53a
jayzer0098 <jayzer0098@gmail.com>
2015-08-28 14:24:47 +0800

    Bower - Datepicker
    Install bower

--- resources/views/dashboard/add.blade.php
+++ resources/views/dashboard/add.blade.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/responsive.dataTables.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/responsive.foundation.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/responsive.jqueryui.min.css') }}">
+<link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>  
 <link rel="stylesheet" type="text/css" href= "{{ asset('css/style.css') }}">
@@ -117,6 +118,17 @@
                     </div>
                   </div>
                   <div class="form-group">
+                    <label for="select" class="col-lg-2 control-label">Warranty End</label>
+                    <div class="col-lg-10">
+                      <div class='input-group date' id='datetimepicker3'>
+                        <input type='text' class="form-control" name="warranty_end" />
+                        <span class="input-group-addon">
+                          <span class="glyphicon glyphicon-time"></span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
                     <div class="col-lg-10 col-lg-offset-2">
                       <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
@@ -149,6 +161,8 @@
 @section('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
+<script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
 <script type="text/javascript" src="{{ asset('js/daTables.responsive.min.js') }}"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.8/js/dataTables.bootstrap.min.js"></script>
 <script type="text/javascript">
@@ -156,6 +170,9 @@ $(document).ready(function(){
     $('#assetTable').DataTable();
     $('#assetTable1').DataTable();
     $('#assetTable2').DataTable();
+    $('#datetimepicker3').datetimepicker({
+      format: 'YYYY-MM-DD'
+    });
 });
 </script>
 @stop
