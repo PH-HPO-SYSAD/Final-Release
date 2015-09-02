b36237f2d55367ea0bfb019545a133a670274319
jayzer0098 <jayzer0098@gmail.com>
2015-09-02 13:17:40 +0800

    account information

--- resources/views/dashboard/account.blade.php
+++ resources/views/dashboard/account.blade.php
@@ -11,21 +11,52 @@
 <link rel="stylesheet" type="text/css" href= "{{ asset('css/style.css') }}">
 @stop  
 @section('contents')
+
+{{-- account information --}}
     <div class="row">
     	<div class="col-md-4">
 			<div class="list-group">
-			  <a href="#" class="list-group-item active">
-			    Account Information
-			  </a>
-			  <a href="#" class="list-group-item">Name:&nbsp&nbsp{{ auth()->user()->name }}
-			  </a>
-			  <a href="#" class="list-group-item">Username:&nbsp&nbsp{{ auth()->user()->username }}
-			  </a>
-			   <a href="#" class="list-group-item">Account Type:&nbsp&nbsp{{ auth()->user()->account_type }}
-			  </a>
+			  <p class="list-group-item active">
+			  	Account Information
+			  </p>
+			  <p  class="list-group-item">Name:&nbsp&nbsp{{ auth()->user()->name }}
+			  </p>
+			  <p  class="list-group-item">Username:&nbsp&nbsp{{ auth()->user()->username }} <a href="#"  data-toggle="modal" data-target="#edit" class="btn btn-sm btn-primary">edit</a>
+			  </p>
+			   <p class="list-group-item">Account Type:&nbsp&nbsp{{ auth()->user()->account_type }}
+			  </p>
 			</div>
     	</div>
     </div>
+
+{{-- modal fade (Edit Username) --}}
+<div class="modal fade" id="edit">
+    <div class="modal-dialog">
+         <form class="form-horizontal">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title">Edit Username</h4>
+              </div>
+              <div class="modal-body">
+                <fieldset>
+                  <legend>Edit Information below</legend>
+                  <div class="form-group">
+                    <label for="select" class="col-lg-2 control-label">New Username</label>
+                    <div class="col-lg-10">
+                    	<input type="text" class="form-control" id="username" name="edit_username" placeholder="New Username">
+                    </div>
+                  </div>
+                </fieldset>
+              </div>
+              <div class="modal-footer">
+               <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
 @section('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
